8c4c1545c3c26553cd374281bdba20c8446ad0b1
turkishman41 <89349479+turkishman41@users.noreply.github.com>
2022-07-17 20:15:48 +0430

    Update update.py

--- update.py
+++ update.py
@@ -44,7 +44,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/HuzunluArtemis/MirrorLeechTelegramBot"
+    UPSTREAM_REPO = "https://github.com/turkishman41/MirrorLeechTelegramBot"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
